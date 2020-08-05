4093efee1e389d52bbe02e26aa8fb1710b0d65a3
THE-ORONCO <theo.roncoletta@gmail.com>
2020-08-05 10:26:29 +0200

    created some ObjectManagement-Stuff but before finishing it I need confirmation from Kai

--- src/main/java/Museum/ObjectManagement/MuseumsManager.java
+++ src/main/java/Museum/ObjectManagement/MuseumsManager.java
@@ -1,4 +1,55 @@
 package Museum.ObjectManagement;
 
+import java.util.ArrayList;
+
 public class MuseumsManager {
+    private PersonenManager personenM;
+    private RaumManager raumM;
+    private ExponatManager exponatM;
+
+    public MuseumsManager(PersonenManager personenM, RaumManager raumM, ExponatManager exponatM) {
+        this.personenM = personenM;
+        this.raumM = raumM;
+        this.exponatM = exponatM;
+    }
+
+    public PersonenManager getPersonenM() {
+        return personenM;
+    }
+
+    public void setPersonenManager(PersonenManager personenM) {
+        this.personenM = personenM;
+    }
+
+    public RaumManager getRaumManager() {
+        return raumM;
+    }
+
+    public void setRaumManager(RaumManager raumM) {
+        this.raumM = raumM;
+    }
+
+    public ExponatManager getExponatManager() {
+        return exponatM;
+    }
+
+    public void setExponatManger(ExponatManager exponatM) {
+        this.exponatM = exponatM;
+    }
+
+    public boolean importieren(String path, Class importetType) {//TODO Spezifikation der Methodensignatur
+        //TODO SWE-Tools benutzen um dat zu importieren
+        return false;
+    }
+
+    // TODO vielleicht sollte man die export-Methoden für die einzelnen Elementtypen aufteilen
+    // Standartmethode exportiert im CSV-Format
+    public boolean exportieren(String path, ArrayList<Object> exportedElements, boolean ueberschreiben) {
+        return this.exportieren(path, exportedElements, ueberschreiben, "CSV");
+    }
+
+    public boolean exportieren(String path, ArrayList<Object> exportedElements, boolean ueberschreiben, String exportType) {
+        // TODO SWE-Tools benutzen um dat zu exportieren
+        return false;
+    }
 }
--- src/main/java/Museum/ObjectManagement/PersonenManager.java
+++ src/main/java/Museum/ObjectManagement/PersonenManager.java
@@ -1,4 +1,23 @@
 package Museum.ObjectManagement;
 
-public class PersonenManager {
+public class PersonenManager implements ElementManager{
+    public boolean contains(Object element) {
+        return false;
+    }
+
+    public boolean persist(Object element) {
+        return false;
+    }
+
+    public boolean remove(Object element) {
+        return false;
+    }
+
+    public Object find(String id) {
+        return null;
+    }
+
+    public boolean edit(Object element) {
+        return false;
+    }
 }
